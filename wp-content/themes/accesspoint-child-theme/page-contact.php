0b85e8732513640738acbd50b02bedb3bcdc8782
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-18 09:58:13 +0100

    Refactor office locations section in contact page template

--- wp-content/themes/accesspoint-child-theme/page-contact.php
+++ wp-content/themes/accesspoint-child-theme/page-contact.php
@@ -5,16 +5,27 @@
 ?>
 
 
-<section class="office-locations">
-    <div class="office-locations__inner">
-        <?php if (have_rows('office_locations') ) :?>
-                <?php while (have_rows('office_locations') ) : the_row(); ?>
-                    test
-                <?php endwhile ; ?>
-        <?php endif ; ?>
 
-    </div>
-</section>
+    <?php if (have_rows('office_locations') ) :?>
+        <section class="office-locations">
+            <div class="office-locations__inner">
+                <?php while (have_rows('office_locations') ) : the_row(); 
+                    $office_name = get_sub_field('location_name');
+                ?>
+                    <div class="office-locations__inner--content">  
+                        <?php if($office_name) : ?>
+                            <h2 class="office-locations__inner--office-name"><?php echo ($office_name) ?></h2>
+                        <?php endif; ?>
+
+                    </div>
+                    <div class="office-locations__inner--map">
+
+
+                    </div>
+                <?php endwhile ; ?>
+            </div>
+        </section>
+    <?php endif ; ?>
 
 
 <?php
