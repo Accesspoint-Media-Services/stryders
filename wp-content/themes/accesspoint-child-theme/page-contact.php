73e035c60cf838eeb4ec9c9f38804a1d19503086
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-18 10:34:40 +0100

    added guttenberg page blocks

--- wp-content/themes/accesspoint-child-theme/page-contact.php
+++ wp-content/themes/accesspoint-child-theme/page-contact.php
@@ -1,64 +1,68 @@
-<?php 
-    /* Template Name: Contact */
+<?php
 
-    get_header(); 
+/* Template Name: Contact */
+
+get_header();
 ?>
 
-<?php if (have_rows('office_locations') ) :?>
+<?php while ( have_posts() ) : the_post(); ?>
+    <?php the_content(); ?>
+<?php endwhile; ?>
+
+<?php if ( have_rows( 'office_locations' ) ) : ?>
     <section class="office-locations container">
-        
-            <?php while (have_rows('office_locations') ) : the_row(); 
-                $office_name = get_sub_field('location_name');
-                $content = get_sub_field('content');
-                $address = get_sub_field('address');
-                $phone = get_sub_field('phone_number');
-                $google_map = get_sub_field('google_map');
-            ?>
-                <?php if( $office_name or $address or $content or $google_map ) : ?>
-                    <div class="office-locations__item has-primary-background-color">
-                        
-                        <?php if( $office_name or $address or $content ) : ?>
-                            <div class="office-locations__inner--content">  
-                                <?php if($office_name) : ?>
-                                    <h2 class="office-locations__inner--office-name"><?php echo ($office_name); ?></h2>
-                                <?php endif; ?>
 
-                                <?php if($content) : ?>
-                                    <div class="office-locations__inner--copy">
-                                        <?php echo ($content); ?>
-                                    </div>
-                                <?php endif; ?>
+        <?php while ( have_rows( 'office_locations' ) ) : the_row();
+            $office_name = get_sub_field( 'location_name' );
+            $content     = get_sub_field( 'content' );
+            $address     = get_sub_field( 'address' );
+            $phone       = get_sub_field( 'phone_number' );
+            $google_map  = get_sub_field( 'google_map' );
+        ?>
+            <?php if ( $office_name || $address || $content || $google_map ) : ?>
+                <div class="office-locations__item has-primary-background-color">
 
-                                <?php if($address or $phone) : ?>
-                                    <div class="office-locations__inner--address">
-                                        <?php echo ($address); ?>
+                    <?php if ( $office_name || $address || $content ) : ?>
+                        <div class="office-locations__inner--content">
+                            <?php if ( $office_name ) : ?>
+                                <h2 class="office-locations__inner--office-name"><?php echo esc_html( $office_name ); ?></h2>
+                            <?php endif; ?>
 
-                                        <?php if($phone) : ?>
-                                            <div class="office-locations__inner--phone">
-                                                <a href="tel:<?php echo($phone)?>"><span class="helper">T:</span> <?php echo($phone) ; ?></a>
-                                            </div>
-                                        <?php endif; ?>
+                            <?php if ( $content ) : ?>
+                                <div class="office-locations__inner--copy">
+                                    <?php echo wp_kses_post( $content ); ?>
+                                </div>
+                            <?php endif; ?>
 
-                                    </div>
+                            <?php if ( $address || $phone ) : ?>
+                                <div class="office-locations__inner--address">
+                                    <?php echo esc_html( $address ); ?>
 
+                                    <?php if ( $phone ) : ?>
+                                        <div class="office-locations__inner--phone">
+                                            <a href="tel:<?php echo esc_attr( preg_replace( '/[^0-9+]/', '', $phone ) ); ?>">
+                                                <span class="helper">T:</span> <?php echo esc_html( $phone ); ?>
+                                            </a>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
+                            <?php endif; ?>
 
-                                <?php endif; ?>
-
-                            </div>
-                        <?php endif; ?>
-
-                        <?php if($google_map) : ?>
-                            <div class="office-locations__inner--map">
-                                <?php echo ($google_map); ?>
-                            </div>
-                        <?php endif; ?>
+                        </div>
                     <?php endif; ?>
-                </div>
-            <?php endwhile ; ?>
-        
-    </section>
-<?php endif ; ?>
 
+                    <?php if ( $google_map ) : ?>
+                        <div class="office-locations__inner--map">
+                            <?php echo ( $google_map ); ?>
+                        </div>
+                    <?php endif; ?>
+
+                </div>
+            <?php endif; ?>
+        <?php endwhile; ?>
+
+    </section>
+<?php endif; ?>
 
 <?php
 get_footer();
