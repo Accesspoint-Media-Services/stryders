6db8066b0242356a51dfbbcb6761d711b9e84b89
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-18 09:53:39 +0100

    repeater field added

--- wp-content/themes/accesspoint-child-theme/page-contact.php
+++ wp-content/themes/accesspoint-child-theme/page-contact.php
@@ -5,6 +5,17 @@
 ?>
 
 
+<section class="office-locations">
+    <div class="office-locations__inner">
+        <?php if (have_rows('office_locations') ) :?>
+                <?php while (have_rows('office_locations') ) : the_row(); ?>
+                    test
+                <?php endwhile ; ?>
+        <?php endif ; ?>
+
+    </div>
+</section>
+
 
 <?php
 get_footer();
