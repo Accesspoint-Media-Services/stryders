829e9473f5c632c78de1e895c111511b95a4f940
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-18 10:05:25 +0100

    structure changes

--- wp-content/themes/accesspoint-child-theme/page-contact.php
+++ wp-content/themes/accesspoint-child-theme/page-contact.php
@@ -6,7 +6,7 @@
 
 <?php if (have_rows('office_locations') ) :?>
     <section class="office-locations">
-        <div class="office-locations__inner">
+        
             <?php while (have_rows('office_locations') ) : the_row(); 
                 $office_name = get_sub_field('location_name');
                 $content = get_sub_field('content');
@@ -14,24 +14,28 @@
                 $phone = get_sub_field('phone_number');
                 $google_map = get_sub_field('google_map');
             ?>
-                <div class="office-locations__inner--content">  
-                    <?php if($office_name) : ?>
-                        <h2 class="office-locations__inner--office-name"><?php echo ($office_name); ?></h2>
-                    <?php endif; ?>
-                    <?php if($content) : ?>
-                    <div class="office-locations__inner--copy">
-                        <?php echo ($content); ?>
-                    </div>
-                    <?php endif; ?>
+                <?php if( $office_name or $address or $content or $google_map ) ?>
+                    <div class="office-locations__item has-primary-background-color">
+                        <div class="office-locations__inner--content">  
+                            <?php if($office_name) : ?>
+                                <h2 class="office-locations__inner--office-name"><?php echo ($office_name); ?></h2>
+                            <?php endif; ?>
+                            <?php if($content) : ?>
+                            <div class="office-locations__inner--copy">
+                                <?php echo ($content); ?>
+                            </div>
+                            <?php endif; ?>
 
+                        </div>
+                        <?php if($google_map) : ?>
+                        <div class="office-locations__inner--map">
+                            <?php echo ($google_map); ?>
+                        </div>
+                        <?php endif; ?>
+                    <?php endif; ?>
                 </div>
-                <?php if($google_map) : ?>
-                <div class="office-locations__inner--map">
-                    <?php echo ($google_map); ?>
-                </div>
-                <?php endif; ?>
             <?php endwhile ; ?>
-        </div>
+        
     </section>
 <?php endif ; ?>
 
